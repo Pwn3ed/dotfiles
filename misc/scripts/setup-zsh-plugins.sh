e2d80e6faf79cebcc030d19d314aa34c2fd98865
leonardo-luz <leonardoluz.f02@gmail.com>
2025-05-03 00:40:59 -0300

    feat: update zsh plugins setup script

--- misc/scripts/setup-zsh-plugins.sh
+++ misc/scripts/setup-zsh-plugins.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; then
+# Check if oh-my-zsh is installed
+if [ -z "$ZSH_CUSTOM" ]; then
+  echo "oh-my-zsh not installed!"
+  exit 1
+fi
+
+# Check and install zsh-syntax-highlighting
+if [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
   echo "zsh-syntax-highlighting already installed"
 else
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 fi
 
-if ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions; then
+# Check and install zsh-autosuggestions
+if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
   echo "zsh-autosuggestions already installed"
 else
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 fi
 
-echo "in ~/.zshrc change ZSH_THEME to bureau"
-echo "in ~/.zshrc add zsh-autosuggestions zsh-syntax-highlighting to plugins"
+echo "In ~/.zshrc change ZSH_THEME to 'bureau'"
+echo "In ~/.zshrc add 'zsh-autosuggestions zsh-syntax-highlighting' to plugins"
