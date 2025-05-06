115fbca97689d0efb7a5f623238057bc74ddd796
leonardo-luz <leonardoluz.f02@gmail.com>
2025-05-06 17:29:21 -0300

    feat: update scripts

--- misc/scripts/setup-arch-env.sh
+++ misc/scripts/setup-arch-env.sh
@@ -166,7 +166,7 @@ else
     echo "==> rustup not found. Skipping Rust setup."
 fi
 
-if [ -d "$HOME/.config/plugins/catppuccin/tmux" ]; then
+if [ -d "$HOME/.config/tmux/plugins/catppuccin/tmux" ]; then
   echo "Catppuccin theme is already installed. Skipping."
 else
   git clone https://github.com/omerxx/catppuccin-tmux.git ~/.config/tmux/plugins/catppuccin/tmux
--- misc/scripts/setup-zsh-plugins.sh
+++ misc/scripts/setup-zsh-plugins.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Check if oh-my-zsh is installed
-if [ -z "$ZSH_CUSTOM" ]; then
+if [ -d "$ZSH" ]; then
+  # Check and install zsh-syntax-highlighting
+  if [ -d "$ZSH/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo "zsh-syntax-highlighting already installed"
+  else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH}/custom/plugins/zsh-syntax-highlighting"
+  fi
+
+  # Check and install zsh-autosuggestions
+  if [ -d "$ZSH/custom/plugins/zsh-autosuggestions" ]; then
+    echo "zsh-autosuggestions already installed"
+  else
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH}/custom/plugins/zsh-autosuggestions"
+  fi
+
+  echo "In ~/.zshrc change ZSH_THEME to 'bureau'"
+  echo "In ~/.zshrc add 'zsh-autosuggestions zsh-syntax-highlighting' to plugins"
+else
   echo "oh-my-zsh not installed!"
   exit 1
 fi
 
-# Check and install zsh-syntax-highlighting
-if [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-  echo "zsh-syntax-highlighting already installed"
-else
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-fi
-
-# Check and install zsh-autosuggestions
-if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-  echo "zsh-autosuggestions already installed"
-else
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-fi
-
-echo "In ~/.zshrc change ZSH_THEME to 'bureau'"
-echo "In ~/.zshrc add 'zsh-autosuggestions zsh-syntax-highlighting' to plugins"
