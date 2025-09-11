f4a2bd7dd7e2978f0a6d637391e1ef69b2468d44
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-09-10 21:15:44 -0300

    feat: add ghostty install script

---
+++ scripts/stand-alone/ghostty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo pacman -S gtk4 gtk4-layer-shell libadwaita blueprint-compiler gettext
+
+git clone https://github.com/ghostty-org/ghostty
+
+cd ghostty
+
+zig build -Doptimize=ReleaseFast
+
+cd ..
+
+rm -rf ghostty
